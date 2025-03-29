617e18f08a069a89922273b87d233f1a029d01f7
BTDCBUUS <107370285+BTDCBUUS1@users.noreply.github.com>
2025-03-29 12:46:51 +0000

    Update Areafarm.lua

--- Areafarm.lua
+++ Areafarm.lua
@@ -1,3 +1,19 @@
-local locations = {};
+local locations = {}
 
-return locations;
+locations.AddTable = function(arg, args)
+    table.insert(arg, args)
+end
+
+locations.NewTable = function()
+    return {}
+end
+
+locations.GetTableLength = function(arg)
+    return #arg
+end
+
+locations.GetTableElement = function(arg, index)
+    return arg[index]
+end
+
+return locations
